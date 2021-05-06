04db530b703829d964d18eeb9249b887e01e4d88
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2021-05-06 19:06:07 -0300

    Adicionando arquivo javaScritp.

---
+++ script.js
@@ -0,0 +1,28 @@
+let tituloVoto = document.querySelector('.sup-left-1 span');
+let cargo = document.querySelector('.sup-left-2 span');
+let descricao = document.querySelector('.sup-left-4');
+let aviso = document.querySelector('.tela-inferior');
+let conteudoLateral = document.querySelector ('.tela-superior-right');
+let numeros = document.querySelector ('.sup-left-3');
+
+
+function clicou (n) {
+    alert("Clicou em " +n);
+}
+
+function branco() {
+    alert("Clicou em BRANCO");
+}
+
+function corrige() {
+    alert("Clicou em CORRIGE");
+}
+
+function confirma() {
+    alert("Clicou em CONFIRMA");
+}
+
+/*function clicou(n) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('    ').innerHTML = alert("Clicou em" +n);
+}*/
