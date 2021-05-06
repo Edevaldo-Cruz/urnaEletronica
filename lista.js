2f30330d1751dda7ba4cd91dca86ef342eab69c3
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2021-05-05 21:17:59 -0300

    Adicionando lista de candidatos e novas imagens.

---
+++ lista.js
@@ -0,0 +1,73 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numero: 5,
+        candidatos: [
+        {
+            numero: 12345,
+            nome: "João João",
+            partido: "PZZ",
+            fotos: [
+                {url: "caric3.jpg", legenda: "Vereador", small: false}
+        ]
+
+        },
+        {
+            numero: 71717,
+            nome: "Seu Zé",
+            partido: "PYY",
+            fotos: [
+                {url: "caric4.jpg", legenda: "Vereador", small: false}
+            ]
+    
+        },
+        {
+            numero: 91919,
+            nome: "Seu Madruga",
+            partido: "PFF",
+            fotos: [
+                {url: "caric5.jpg", legenda: "Vereador", small: false}
+            ]
+    
+        },
+        {
+            numero: 71717,
+            nome: "Seu Barriga",
+            partido: "PYY",
+            fotos: [
+                {url: "caric4.jpg", legenda: "Vereador", small: false}
+            ]
+    
+        },
+    ]
+    },
+    {
+        titulo: "PREFEITO",
+        numero: 2,
+        candidatos: [
+        {
+            numero:99,
+            name: "Mr Bean",
+            Partido: "KKK",
+            Vice: "Kiko Tesouro",
+            fotos: [
+                {url: "caric1.jpg", legenda: "Prefeito"},
+                {url: "caric2.jpg", legenda: "Vice-Prefeito", small: true},
+            ]
+        },
+        {
+            numero: 22,
+            name: "Prof. Raimundo",
+            Partido: "RSRS",
+            Vice: "Zé Bonitinho",
+            fotos: [
+                {url: "caric7.jpg", legenda: "Prefeito"},
+                {url: "caric8.jpg", legenda: "Vice-Prefeito", small: true},
+            ]   
+    
+        },
+        ]
+    }
+
+
+]
