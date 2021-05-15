6a9880ae6c6abb5f1fcd9c180d3ffc66374de2e7
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2021-05-15 16:13:36 -0300

    Finalização do projeto.

--- etapas.js
+++ etapas.js
@@ -1,73 +1,58 @@
 let etapas = [
-    {
-        titulo: "VEREADOR",
-        numero: 5,
-        candidatos: [
-        {
-            numero: "12345",
-            nome: "João João",
-            partido: "PZZ",
-            fotos: [
-                {url: "caric3.jpg", legenda: "Vereador", small: false}
-        ]
-
-        },
-        {
-            numero: "71717",
-            nome: "Seu Zé",
-            partido: "PYY",
-            fotos: [
-                {url: "caric4.jpg", legenda: "Vereador", small: false}
-            ]
-    
-        },
-        {
-            numero: "91919",
-            nome: "Seu Madruga",
-            partido: "PFF",
-            fotos: [
-                {url: "caric5.jpg", legenda: "Vereador", small: false}
-            ]
-    
-        },
-        {
-            numero: "71717",
-            nome: "Seu Barriga",
-            partido: "PYY",
-            fotos: [
-                {url: "caric4.jpg", legenda: "Vereador", small: false}
-            ]
-    
-        },
-    ]
-    },
-    {
-        titulo: "PREFEITO",
-        numero: 2,
-        candidatos: [
-        {
-            numero: "99",
-            name: "Mr Bean",
-            Partido: "KKK",
-            Vice: "Kiko Tesouro",
-            fotos: [
-                {url: "caric1.jpg", legenda: "Prefeito"},
-                {url: "caric2.jpg", legenda: "Vice-Prefeito", small: true},
-            ]
-        },
-        {
-            numero: "22",
-            name: "Prof. Raimundo",
-            Partido: "RSRS",
-            Vice: "Zé Bonitinho",
-            fotos: [
-                {url: "caric7.jpg", legenda: "Prefeito"},
-                {url: "caric8.jpg", legenda: "Vice-Prefeito", small: true},
-            ]   
-    
-        },
-        ]
-    }
-
-
-]
+  {
+    titulo: "VEREADOR",
+    numero: 5,
+    candidatos: [
+      {
+        numero: "12345",
+        nome: "João João",
+        partido: "PZZ",
+        foto: [{ url: "caric3.jpg", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: "71717",
+        nome: "Seu Zé",
+        partido: "PYY",
+        foto: [{ url: "caric4.jpg", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: "91919",
+        nome: "Seu Madruga",
+        partido: "PFF",
+        foto: [{ url: "caric5.jpg", legenda: "Vereador", small: false }],
+      },
+      {
+        numero: "71717",
+        nome: "Seu Barriga",
+        partido: "PYY",
+        foto: [{ url: "caric4.jpg", legenda: "Vereador", small: false }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numero: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Mr Bean",
+        partido: "KKK",
+        Vice: "Kiko Tesouro",
+        foto: [
+          { url: "caric1.jpg", legenda: "Prefeito" },
+          { url: "caric2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "22",
+        nome: "Prof. Raimundo",
+        partido: "RSRS",
+        Vice: "Zé Bonitinho",
+        foto: [
+          { url: "caric7.jpg", legenda: "Prefeito" },
+          { url: "caric8.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
--- script.js
+++ script.js
@@ -1,78 +1,130 @@
-let tituloVoto = document.querySelector('.sup-left-1 span');
-let cargo = document.querySelector('.sup-left-2 span');
-let descricao = document.querySelector('.sup-left-4');
-let aviso = document.querySelector('.tela-inferior');
-let conteudoLateral = document.querySelector ('.tela-superior-right');
-let numero = document.querySelector ('.sup-left-3');
+let tituloVoto = document.querySelector(".sup-left-1 span");
+let cargo = document.querySelector(".sup-left-2 span");
+let descricao = document.querySelector(".sup-left-4");
+let aviso = document.querySelector(".tela-inferior");
+let conteudoLateral = document.querySelector(".tela-superior-right");
+let numero = document.querySelector(".sup-left-3");
 
 let etapaAtual = 0;
-let numeros = '';
+let numeros = "";
+let votoBranco = true;
+let votos = []
 
-function inicio () {
-    let etapa = etapas[etapaAtual];
+function inicio() {
+  let etapa = etapas[etapaAtual];
 
-    let numeroHtml = '';
-    
-    for(let i=0;i<etapa.numero;i++) {
-        if(i === 0) {
-            numeroHtml += '<div class="numero pisca"></div>';
-        } else {
-            numeroHtml += '<div class="numero"></div>';
-        }
+  numeros = "";
+  votoBranco = false;
+
+  let numeroHtml = "";
+
+  for (let i = 0; i < etapa.numero; i++) {
+    if (i === 0) {
+      numeroHtml += '<div class="numero pisca"></div>';
+    } else {
+      numeroHtml += '<div class="numero"></div>';
     }
+  }
 
-    tituloVoto.style.display = 'none';
-    cargo.innerHTML = etapa.titulo; 
-    descricao.innerHTML = '';
-    aviso.style.display = 'none';
-    conteudoLateral.innerHTML = '';
-    numero.innerHTML = numeroHtml;
-} 
+  tituloVoto.style.display = "none";
+  cargo.innerHTML = etapa.titulo;
+  descricao.innerHTML = "";
+  aviso.style.display = "none";
+  conteudoLateral.innerHTML = "";
+  numero.innerHTML = numeroHtml;
+}
 
 function atualiza() {
-    let etapa = etapas[etapaAtual];
+  let etapa = etapas[etapaAtual];
 
-    let candidato = etapa.candidatos.filter((item)=>{
-        if(item.numero === numeros) {
-            return true;
-        }else{
-            return false;
-        }
-    });
-
-    console.log ("canditado", candidato);
-}
-
-function clicou (n) {
-    let elNumero = document.querySelector ('.numero.pisca');
-    if (elNumero !== null){
-        elNumero.innerHTML = n;
-        numeros = `${numeros}${n}`;
-
-        elNumero.classList.remove('pisca');
-        if (elNumero.nextElementSibling !== null) {
-            elNumero.nextElementSibling.classList.add('pisca');
-        } else {
-            atualiza();
-            
-        }
+  let candidato = etapa.candidatos.filter((item) => {
+    if (item.numero === numeros) {
+      return true;
+    } else {
+      return false;
     }
-    
-    
+  });
 
+  if (candidato.length > 0) {
+    candidato = candidato[0];
+    tituloVoto.style.display = "block";
+    aviso.style.display = "block";
+    descricao.innerHTML = `Nome: ${candidato.nome}<br/> Partido: ${candidato.partido}`;
+
+    let fotoHtml = "";
+    for (let i in candidato.foto) {
+      fotoHtml += `<div class="img"><img src="img/${candidato.foto[i].url}" alt="Candidato">${candidato.foto[i].legenda}</div>`;
+    }
+
+    conteudoLateral.innerHTML = fotoHtml;
+  } else {
+    tituloVoto.style.display = "block";
+    aviso.style.display = "block";
+    descricao.innerHTML = '<div class ="voto-nulo ocilando">VOTO NULO</div>';
+  }
 }
 
+function clicou(n) {
+  let elNumero = document.querySelector(".numero.pisca");
+  if (elNumero !== null) {
+    elNumero.innerHTML = n;
+    numeros = `${numeros}${n}`;
+
+    elNumero.classList.remove("pisca");
+    if (elNumero.nextElementSibling !== null) {
+      elNumero.nextElementSibling.classList.add("pisca");
+    } else {
+      atualiza();
+    }
+  }
+}
 
 function branco() {
-    alert("Clicou em BRANCO");
+  if (numeros === "") {
+    votoBranco = true;
+    tituloVoto.style.display = "block";
+    aviso.style.display = "block";
+    numero.innerHTML = "";
+    descricao.innerHTML =
+      '<div class ="voto-branco ocilando">VOTO EM BRANCO</div>';
+  } else {
+      alert ("Para votar em branco, você não pode ter digitado nenhum numero.");
+  }
 }
 
 function corrige() {
-    alert("Clicou em CORRIGE");
+  inicio();
 }
 
 function confirma() {
-    alert("Clicou em CONFIRMA");
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push ({
+            etapa: etapas [etapaAtual].titulo,
+            voto: 'branco'
+        });
+        console.log ("Confirmando como Brnaco");
+    }else if(numeros.length === etapa.numero) {
+        votoConfirmado = true;
+        votos.push ({
+            etapa: etapas [etapaAtual].titulo,
+            voto: numeros
+        });
+    }
+
+    if (votoConfirmado) {
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined) {
+            inicio();
+        } else {
+            document.querySelector(".tela").innerHTML = '<div class ="avisoFim ocilando">FIM</div>';
+            console.log (votos);
+        }
+    }
 }
 
-inicio ();
+
+inicio();
